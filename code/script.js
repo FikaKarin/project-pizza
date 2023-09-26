dff03e61dd17fc6a08ae346acb3651c87e407d0f
FikaKarin <mailtolundqvist@gmail.com>
2023-09-26 22:49:52 +0200

    step 2

--- code/script.js
+++ code/script.js
@@ -4,13 +4,31 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
 let name1 = prompt('What is your name?');
 
 alert(`Hi, ${name1}!`);
 
 // Step 2 - Food choice
 // Your code goes here
+
+const pi = 'Pizza';
+const pa = 'Pasta';
+const sa = 'Sallad';
+
+let foodOpt = parseInt(
+  prompt(
+    `Insert the number of what type of food you´d like:\n1. ${pi}\n2. ${pa}\n3. ${sa}`
+  )
+);
+
+if (foodOpt === 1) {
+  alert('You want Pizza!');
+} else if (foodOpt === 2) {
+  alert('You want Pasta!');
+} else if (foodOpt === 3) {
+  alert('You want Sallad!');
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
