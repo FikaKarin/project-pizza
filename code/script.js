2f40d4a67c031d7eac1098f33c5b4ce814893d57
FikaKarin <mailtolundqvist@gmail.com>
2023-09-27 09:51:29 +0200

    Step 3

--- code/script.js
+++ code/script.js
@@ -32,6 +32,59 @@ if (foodOpt === 1) {
 
 // Step 3 - Subtype choice
 // Your code goes here
+let caprisciosa = 'Caprisciosa';
+let calzone = 'Calzone';
+let hawaii = 'Hawaii';
+let carbonara = 'Carbonara';
+let bolognese = 'Bolognese';
+let lasagna = 'Lasagna';
+let ceasar = 'Ceasar';
+let greek = 'Greek';
+let schrimp = 'Schrimp';
+
+if (foodOpt === '1') {
+  let pizzaChoice = 
+    prompt(
+      `Please answer what type of meal you want\n1. ${caprisciosa}\n2. ${calzone}\n3. ${hawaii}`
+    );
+
+  if (pizzaChoice === '1') {
+    alert(`You want ${caprisciosa}`);
+  } else if (pizzaChoice === '2') {
+    alert(`You want ${calzone}`);
+  } else if (pizzaChoice === '3') {
+    alert(`You want ${hawaii}`);
+  }
+} else if (foodOpt === '2') {
+  let pastaChoice = prompt(
+    `Please answer what type of meal you want\n1. ${carbonara}\n2. ${bolognese}\n3. ${lasagna}`
+  );
+
+  if (pastaChoice === '1') {
+    alert(`You want ${carbonara}`);
+  } else if (pizzaChoice === '2') {
+    alert(`You want ${bolognese}`);
+  } else if (pizzaChoice === '3') {
+    alert(`You want ${lasagna}`);
+  }
+  
+} else if (foodOpt === '3') {
+  let salladChoice = prompt(
+    `Please answer what type of meal you want\n1. ${ceasar}\n2. ${greek}\n3. ${schrimp}`
+  );
+
+  if (salladChoice === 1) {
+    alert(`You want ${ceasar}`);
+  } else if (salladChoice === '2') {
+    alert(`You want ${greek}`);
+  } else if (salladChoice === '3') {
+    alert(`You want ${schrimp}`);
+  } else {
+    alert(`Invalid choice for salad: ${salladChoice}`);
+  }
+} else {
+  alert('Invalid food option. Please choose 1, 2, or 3.');
+}
 
 // Step 4 - Age
 // Your code goes here
